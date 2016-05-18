4e9c4929feed1577baf2d77c94bbeeeba4cb049a
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-05-18 09:59:29 -0500

    [SQUASH] moar updtates from robin
    https://github.com/invisiblek/android_kernel_motorola_msm8992/commits/cm-13.0
    
    Change-Id: I32d5fb93b417cc812fc5b99b2ef0917cb8039c0f

--- drivers/pinctrl/pinctrl-msm-tlmm.c
+++ drivers/pinctrl/pinctrl-msm-tlmm.c
@@ -21,6 +21,7 @@
 #include <linux/pinctrl/pinconf-generic.h>
 #include <linux/spinlock.h>
 #include <linux/syscore_ops.h>
+#include <linux/wakeup_reason.h>
 #include "pinctrl-msm.h"
 
 /* config translations */
@@ -166,6 +167,8 @@
 #define TLMMV4_QDSD_PULL_OFFSET			0x3
 #define TLMMV4_QDSD_CONFIG_WIDTH		0x5
 #define TLMMV4_QDSD_DRV_MASK			0x7
+
+extern int msm_show_resume_irq_mask;
 
 struct msm_sdc_regs {
 	unsigned long pull_mask;
@@ -949,39 +952,37 @@ static int msm_tlmm_gp_irq_suspend(void)
 	return 0;
 }
 
-extern int msm_show_resume_irq_mask;
-
-static void msm_tlmm_gp_show_resume_irq(void)
+void msm_tlmm_show_gp_irq_resume(void)
 {
-	unsigned long i;
-	unsigned int virq = 0;
+	unsigned long irq_flags;
+	int i, irq, intstat;
 	struct msm_tlmm_irq_chip *ic = &msm_tlmm_gp_irq;
-	struct msm_pintype_info *pinfo = ic_to_pintype(ic);
-	struct gpio_chip *gc = pintype_get_gc(pinfo);
+        int num_irqs = ic->num_irqs;
 
 	if (!msm_show_resume_irq_mask)
 		return;
 
-	for_each_set_bit(i, ic->enabled_irqs, ic->num_irqs) {
-		if (msm_tlmm_get_intr_status(ic, i)) {
+	spin_lock_irqsave(&ic->irq_lock, irq_flags);
+	for_each_set_bit(i, ic->wake_irqs, num_irqs) {
+		intstat = msm_tlmm_get_intr_status(ic, i);
+		if (intstat) {
 			struct irq_desc *desc;
 			const char *name = "null";
 
-			virq = msm_tlmm_gp_to_irq(gc, i);
-			if (virq != 0) {
-				desc = irq_to_desc(virq);
-				if (desc == NULL)
-					name = "stray irq";
-				else {
-					desc->wakeup_irqs++;
-					if (desc->action && desc->action->name)
-						name = desc->action->name;
-				}
-				pr_warn("%s: %d triggered %s\n",
-						__func__, virq, name);
+			irq = irq_create_mapping(ic->domain, i);
+
+			log_base_wakeup_reason(irq);
+
+			desc = irq_to_desc(irq);
+			if (desc != NULL &&
+				desc->action && desc->action->name) {
+				name = desc->action->name;
+				pr_warning("%s: %d triggered %s\n",
+						__func__, irq, name);
 			}
 		}
 	}
+	spin_unlock_irqrestore(&ic->irq_lock, irq_flags);
 }
 
 static void msm_tlmm_gp_irq_resume(void)
@@ -991,7 +992,7 @@ static void msm_tlmm_gp_irq_resume(void)
 	struct msm_tlmm_irq_chip *ic = &msm_tlmm_gp_irq;
 	int num_irqs = ic->num_irqs;
 
-	msm_tlmm_gp_show_resume_irq();
+	msm_tlmm_show_gp_irq_resume();
 
 	spin_lock_irqsave(&ic->irq_lock, irq_flags);
 	for_each_set_bit(i, ic->wake_irqs, num_irqs)
